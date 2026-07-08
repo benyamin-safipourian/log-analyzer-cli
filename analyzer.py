58338e281beae6cdd96be20db1d3354217605fe7
benyamin-safipourian <safipourian.benyamin@gmail.com>
2026-07-09 00:56:56 +0330

    implement log analysis reporting

--- analyzer.py
+++ analyzer.py
@@ -1,5 +1,6 @@
 import re
 import sys
+from collections import Counter
 
 log_pattern = r'(?P<ip>\S+) - - \[(?P<time>.*?)\] "(?P<method>\S+) (?P<path>\S+) \S+" (?P<status>\d+) \d+'
 
@@ -14,7 +15,11 @@ def parse_log_line(line):
         return None
 
 
+unique_ips = set()
+endpoint_counts = Counter()
 def procces_log_file(file_path):
+    total_requests = 0
+    malformed_logs = 0
 
     try:
         with open(file_path , "r") as file:
@@ -22,9 +27,25 @@ def procces_log_file(file_path):
                 result = parse_log_line(log.strip())
 
                 if result:
-                    print(f"ip : {result["ip"]} time : {result["time"]} , method : {result["method"]} ,path : {result["path"]} , status : {result["status"]}")
+                    endpoint_counts[result["path"]] += 1
+                    unique_ips.add(result["ip"])
+                    total_requests += 1
                 else:
-                    print("Malformed line")
+                    malformed_logs += 1
+
+        print("\nTop 10 Endpoints:")
+        if endpoint_counts:
+            for path, count in endpoint_counts.most_common(10):
+                print(f"- {path}: {count}")
+        else:
+            print("No endpoints found.")
+
+        print("-" * 40)
+        print(f"total requests : {total_requests}")
+        print(f"malformed logs : {malformed_logs}")
+        print(f"Unique IPs: {len(unique_ips)}")
+        print("-" * 40)
+
     except FileNotFoundError:
         print("file not founded!")
 
